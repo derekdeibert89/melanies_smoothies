e88d47b49c3f96c4587f04c673a185a99ff8af93
derekdeibert89 <149806663+derekdeibert89@users.noreply.github.com>
2024-08-13 11:25:28 -0400

    updated search term

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -38,8 +37,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
-        search_term = my_dataframe[fruit_chosen]
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        search_term = pd_df.loc(pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0])
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_term)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     time_to_insert = st.button('Submit Order')
